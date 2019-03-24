36404f2794aa0f8067505b4c4851899f61e253fa
Michał Głowienko <44148021+lthead@users.noreply.github.com>
2019-03-24 09:21:13 +0100

    Major changes
    1) Changed date readout to YYYY-MM-DD like in grandMA2 console
    2) Changed numbering of earlier files with the same names. Now it is similar to numbering backups on console. File with no revision number is the newest.
    3) Revision number is now inserted before file extension.
    4) Now the script checks if there is a 7-zip installed on machine in default Program Files directory. Also x86. If there is 7-zip x64 or x86 the script will use it, because it is approximately 6 times faster and takes 15% less space. If there is no 7-zip installed, script will use Compress-Archive method.
    5) Extra variable to choose archive extension (if there is no 7-zip it will be automatically changed for *.zip)
    6) MA Lighting folder directory is now taken from default ProgramData directory.
    7) Read-Host line for not closing window after error - it doesn't work every time but it can help in some cases.
    8) Minor changes and comments

--- onpc-backup-script/CreateMA2ShowFolderBackup.ps1
+++ onpc-backup-script/CreateMA2ShowFolderBackup.ps1
@@ -1,36 +1,66 @@
-﻿<#
+<#
         Original Version by: Philipp Börner
         Source: https://freshblog.azurewebsites.net/2016/03/28/backup-you-work-with-powershell/
-
         Adapted by @aGuyNamedJonas for MA2 onPC
-
+        
+        Modified by Michał Głowienko
+        Inspired by:
+        morgb >>> https://stackoverflow.com/questions/39253435/renaming-a-file-name-to-the-next-incremental-number-with-powershell-script/39254176
+        DarkLite1 >>> https://stackoverflow.com/questions/25287994/running-7-zip-from-within-a-powershell-script
         ---------------------------------------------
         SUBSCRIBE      >>> youtube.com/aGuyNamedJonas
         QA & COMMUNITY >>> fb.com/aGuyNamedJonas
         GET IN TOUCH   >>> hi@aGuyNamedJonas.com
         ---------------------------------------------
-
         HOW TO USE:
         - Make sure $source points to your PC's showfolder
         - Change $dest to point to your harddrive / cloud storage folder (=backup folder)
-
+        - Choose $archiveFormat, ".7z" is recommended, but you can use also ".zip", ".tar" and others.
 #>
- 
+
 #Define source and destination folders
-$source = 'C:\ProgramData\MA Lighting Technologies\grandma'
-$dest = 'F:\Google Drive\2019\Showfolder Backups\'
- 
+$source = "$env:ProgramData\MA Lighting Technologies\grandma"
+$dest = "D:\onPC Backup Folder"
+$archiveFormat = ".7z"
+
 #Getting the current date
-$date = Get-Date -UFormat _%d_%m_%Y
- 
-#Building ZIP file name and complete backup path
-$ZipFile = 'MA2ShowFolderBackup'+$date+'.zip'
-$BackupPath = $dest+$ZipFile
- 
-#creating GUID if ZIP archive already exist, to rename the pre existing file
-$GUID = [GUID]::NewGuid().ToString()
-$doubleBackup = $ZipFile+$GUID
-if (Test-Path $BackupPath) {Rename-Item $BackupPath -NewName $doubleBackup}
- 
-#Creating ZIP archive
-Get-Item -Path $source | Compress-Archive -DestinationPath $BackupPath -CompressionLevel Optimal -Verbose
+$date = Get-Date -UFormat _%Y_%m_%d
+
+#Building archive file name and complete backup path
+$ZipFile = "MAShowFolderBackup" + $date
+$BackupPath = $dest + "\" + $ZipFile + $archiveFormat
+
+#Checking if file name exist. If yes - add a number after "-"
+function AddRevisionNumber([string]$BackupPath)
+{
+    if (Test-Path $BackupPath)
+    {
+        $latest = Get-ChildItem -Path $dest| Sort-Object Name -Descending | Select-Object -First 1
+        #split the latest filename, increment the number, then re-assemble new filename:
+        $newFileName = $latest.BaseName.Split("-")[0] + "-" + ([int]$latest.BaseName.Split("-")[1] + 1).ToString().PadLeft(3,"0") + $latest.Extension
+        Move-Item -path $BackupPath -destination $dest\$newFileName
+    }
+}
+
+#Creating archive with 7-zip if it is installed. If it is not - try to use Compress-Archive method and change to .zip extension.
+if (test-path "$env:ProgramFiles\7-Zip\7z.exe")
+{
+        AddRevisionNumber $BackupPath
+        set-alias sz "$env:ProgramFiles\7-Zip\7z.exe"
+        sz a -mx=9 $BackupPath $source
+}
+elseif (test-path "${env:ProgramFiles(x86)}\7-Zip\7z.exe")
+{
+        AddRevisionNumber $BackupPath
+        set-alias sz "${env:ProgramFiles(x86)}\7-Zip\7z.exe"
+        sz a -mx=9 $BackupPath $source
+}
+else
+{
+        $BackupPath = $dest + "\" + $ZipFile + ".zip"
+        AddRevisionNumber $BackupPath
+        Get-Item -Path $source | Compress-Archive -DestinationPath $BackupPath -CompressionLevel Optimal -Verbose
+}
+
+#Troubleshooting - PowerShell should not close after error
+Read-Host `n`n"PRESS ENTER TO CONTINUE..."
